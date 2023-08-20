06d6769e6e1f8ed335f925ac49943ffec4ac213a
bruno <bruno_am_22@hotmail.com>
2023-08-20 17:54:02 -0300

    porbando

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,8 +1,13 @@
 import {User} from './user';
 import {Auth} from './auth';
 import {Publicar} from './publicacion';
-
+import {SolicitudAmistad} from './solicitud-amistad';
 User.hasOne(Auth);
 User.hasMany(Publicar);
+User.belongsToMany(User, {
+  through: SolicitudAmistad,
+  as: 'solicitante',
+  foreignKey: 'solicitanteId',
+});
 
 export {Auth, User, Publicar};

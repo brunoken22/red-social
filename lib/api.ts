ef905912cc5743e811991c81e60de8109d89802e
bruno <bruno_am_22@hotmail.com>
2023-08-24 11:38:22 -0300

    corrigiendo otro error

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 const Api_url =
-  process.env.NEXT_PUBLIC_API_HOST || 'https://red-social-omega.vercel.app/';
+  process.env.NEXT_PUBLIC_API_HOST || 'https://red-social-omega.vercel.app';
 export async function fetchApiSwr(prop: [string, object]) {
   const api = prop[1] || {};
   const response = await fetch(`${Api_url}/api` + prop[0], api);

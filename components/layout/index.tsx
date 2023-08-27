cce09fb69b953a82ddfd7e983fd31c3328464b6d
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-27 10:13:25 -0300

    mas

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,35 +1,35 @@
 'use client';
 import {StyledComponentsRegistry} from './registry';
 import {Header} from '@/components/header';
-import {ThemeProvider} from 'styled-components';
+// import {ThemeProvider} from 'styled-components';
 import React from 'react';
 import {RecoilRoot} from 'recoil';
 
 export function Layout({children}: {children: React.ReactNode}) {
-  const theme = {
-    default: {
-      bg: '#242526;',
-      color: '#fff',
-      contenedor: '#1c1c1c',
-    },
-  };
+  // const theme = {
+  //   default: {
+  //     bg: '#242526;',
+  //     color: '#fff',
+  //     contenedor: '#1c1c1c',
+  //   },
+  // };
 
   return (
     <RecoilRoot>
       <StyledComponentsRegistry>
-        <ThemeProvider theme={theme.default}>
-          <div
-            style={{
-              minHeight: '100vh',
-              display: 'flex',
-              flexDirection: 'column',
-              backgroundColor: '#1c1c1c',
-              justifyContent: 'space-between',
-            }}>
-            <Header />
-            {children}
-          </div>
-        </ThemeProvider>
+        {/* <ThemeProvider theme={theme.default}> */}
+        <div
+          style={{
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            backgroundColor: '#1c1c1c',
+            justifyContent: 'space-between',
+          }}>
+          <Header />
+          {children}
+        </div>
+        {/* </ThemeProvider> */}
       </StyledComponentsRegistry>
     </RecoilRoot>
   );

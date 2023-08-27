46a5ba91ef5ae4afd9295cb503d1ee747c673d5b
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-27 20:58:44 -0300

    intento

--- lib/models/conn.ts
+++ lib/models/conn.ts
@@ -4,6 +4,11 @@ import pg from 'pg'; // Importa el controlador
 const sequelize = new Sequelize(process.env.SEQUELIZE as string, {
   dialect: 'postgres',
   dialectModule: pg,
+  pool: {
+    max: 1, // Número máximo de conexiones en el pool
+    min: 0, // Número mínimo de conexiones en el pool
+    idle: 10000, // Tiempo máximo (en milisegundos) que una conexión puede estar inactiva en el pool antes de ser eliminada
+  },
 });
 try {
   sequelize.authenticate();

4ac3d63e1e33f8dfd989adb02307bffc99ab110a
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-27 20:52:56 -0300

    intetnto

--- app/api/cerrarSequelize/route.ts
+++ app/api/cerrarSequelize/route.ts
@@ -2,13 +2,13 @@ import {NextRequest, NextResponse} from 'next/server';
 import {sequelize} from '@/lib/models/conn';
 
 export async function GET(request: NextRequest) {
-  //   try {
-  //     sequelize.close().then(() => {
-  //       console.log('Pool cerrado completamente');
-  //     });
-  //     return NextResponse.json('ok.');
-  //   } catch (e) {
-  //     console.log(e);
-  //     return NextResponse.json({message: 'Falta Token'});
-  //   }
+  try {
+    //   sequelize.close().then(() => {
+    //     console.log('Pool cerrado completamente');
+    //   });
+    return NextResponse.json('ok.');
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({message: 'Falta Token'});
+  }
 }

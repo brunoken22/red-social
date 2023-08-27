455be2755824dbadad2ed7cf72caa50e9be664b8
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-27 20:27:17 -0300

    conection sol

--- lib/sync.ts
+++ lib/sync.ts
@@ -1,5 +1,5 @@
 import {sequelize} from './models/conn';
 import './models';
-sequelize.sync({alter: true}).then((res: any) => {
-  console.log('data');
-});
+// sequelize.sync({alter: true}).then((res: any) => {
+//   console.log('data');
+// });

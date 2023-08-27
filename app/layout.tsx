a899269807bd7d74c9755bdae936e5f1b6aa6566
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-27 10:21:57 -0300

    mas

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        {/* <Layout>{children}</Layout> */}
-        {children}
+        <Layout>{children}</Layout>
       </body>
     </html>
   );

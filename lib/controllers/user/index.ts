6355288161d56ebbecc64ca59557b937d858ae87
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-30 13:00:23 -0300

    mas

--- lib/controllers/user/index.ts
+++ lib/controllers/user/index.ts
@@ -141,17 +141,16 @@ export async function getAllAmigos(token: string) {
 export async function getAllUser(token: string) {
   try {
     const tokenData = jwt.verify(token, secrect);
-    // const users = await User.findAll({
-    //   where: {
-    //     id: {
-    //       [Op.ne]: (tokenData as Token).id,
-    //     },
-    //   },
-    // });
-    // if (users) {
-    //   return users;
-    // }
-    console.log(tokenData);
+    const users = await User.findAll({
+      where: {
+        id: {
+          [Op.ne]: (tokenData as Token).id,
+        },
+      },
+    });
+    if (users) {
+      return users;
+    }
     return 'Sin amigos';
   } catch (e) {
     return e;

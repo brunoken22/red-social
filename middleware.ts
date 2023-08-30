06cc179fbb1ffb77a2c60caa4175af38621a57e6
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-29 23:46:02 -0300

    mas

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 
 export default async function middleware(request: NextRequest) {
-  const token = request.headers.get('authorization')!.split(' ')[1];
+  const token = request.headers.get('authorization')?.split(' ')[1];
   if (!token) return NextResponse.json({message: 'Falta Token'});
   const newHeaders = new Headers(request.headers);
   newHeaders.set('token', token);

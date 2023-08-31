f9f25ee5c2c57527b32c20215bc1bb51be1f225a
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-31 20:19:31 -0300

    mas

--- lib/models/conn.ts
+++ lib/models/conn.ts
@@ -1,9 +1,13 @@
 import {Sequelize} from 'sequelize';
 import pg from 'pg'; // Importa el controlador
-
 const sequelize = new Sequelize(process.env.SEQUELIZE as string, {
   dialect: 'postgres',
   dialectModule: pg,
+  pool: {
+    max: 1, // Mantén un solo conjunto de conexión en el pool
+    min: 0,
+    idle: 10000,
+  },
 });
 try {
   sequelize.authenticate();

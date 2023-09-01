f43d871cdeee1ba7abdddf6b9b66deaf4436acdc
Bruno Ken <bruno_am_22@hotmail.com>
2023-09-01 14:11:12 -0300

    mas

--- lib/models/conn.ts
+++ lib/models/conn.ts
@@ -2,13 +2,8 @@ import {Sequelize} from 'sequelize';
 import pg from 'pg';
 
 const sequelize = new Sequelize(process.env.SEQUELIZE as string, {
+  dialect: 'postgres',
   dialectModule: pg,
-  pool: {
-    max: 5, // Número máximo de conexiones en el grupo
-    min: 0, // Número mínimo de conexiones en el grupo
-    acquire: 30000, // Tiempo en milisegundos que una conexión puede esperar antes de lanzar un error
-    idle: 10000, // Tiempo en milisegundos que una conexión puede estar inactiva antes de ser cerrada
-  },
 });
 try {
   sequelize.authenticate();

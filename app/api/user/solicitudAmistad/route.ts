12df1c7c027d69198a73608264128cdc38a93f24
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-31 21:50:51 -0300

    mas

--- app/api/user/solicitudAmistad/route.ts
+++ app/api/user/solicitudAmistad/route.ts
@@ -4,7 +4,6 @@ import {
   getSolicitudAmistad,
   eliminarSolicitud,
 } from '@/lib/controllers/user';
-// import '@/lib/sync';
 
 export async function POST(request: NextRequest) {
   try {
--- lib/models/conn.ts
+++ lib/models/conn.ts
@@ -1,8 +1,5 @@
 const Sequelize = require('sequelize');
-import pg from 'pg'; // Importa el controlador
-const sequelize = new Sequelize(process.env.SEQUELIZE as string, {
-  dialectModule: pg,
-});
+const sequelize = new Sequelize(process.env.SEQUELIZE as string);
 try {
   sequelize.authenticate();
   console.log('Connection has been established successfully.');

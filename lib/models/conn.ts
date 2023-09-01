0d68c0b9c0253cfa3c0cb6dfc52c45189da190af
Bruno Ken <bruno_am_22@hotmail.com>
2023-09-01 09:40:44 -0300

    mas

--- lib/models/conn.ts
+++ lib/models/conn.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.SEQUELIZE as string, {
   dialect: 'postgres',
   dialectModule: pg,
   pool: {
-    max: 10, // Número máximo de conexiones en el grupo
+    max: 1000, // Número máximo de conexiones en el grupo
     min: 0, // Número mínimo de conexiones en el grupo
     idle: 10000, // Tiempo máximo que una conexión puede estar inactiva (en milisegundos)
   },

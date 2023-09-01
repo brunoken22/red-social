0ec0bce7c838b5f157af51b6a42c962d8776b946
Bruno Ken <bruno_am_22@hotmail.com>
2023-08-31 21:33:56 -0300

    mas

--- lib/models/conn.ts
+++ lib/models/conn.ts
@@ -1,8 +1,6 @@
 const {Sequelize, DataTypes, Model} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.SEQUELIZE, {
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(process.env.SEQUELIZE, {});
 
 try {
   sequelize.authenticate();

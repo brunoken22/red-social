334f65971a2516ee748eaadcc14735c239690333
Bruno Ken <bruno_am_22@hotmail.com>
2023-11-27 22:37:20 -0300

    CAMBIANDO URL API

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const Api_url = 'https://red-social-node.vercel.app';
+const Api_url = 'https://red-social-node.onrender.com';
 export async function fetchApiSwr(prop: [string, object]) {
   const api = prop[1] || {};
   const response = await fetch(`${Api_url}/api` + prop[0], api);

976dff0c7be2879feee097a0d3e672e6eb85ec94
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-04 19:16:19 -0300

    probando

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,4 @@
-const Api_url =
-  process.env.NEXT_PUBLIC_PORT || 'https://red-social-node.onrender.com';
+const Api_url = process.env.NEXT_PUBLIC_PORT;
 export async function fetchApiSwr(api: string, option: any) {
   const response = await fetch(`${Api_url}/api${api}`, option);
   const data = await response.json();

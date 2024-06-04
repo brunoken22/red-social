fef01a4773e33f4550c6f4ba2d06ac239af90f11
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-04 16:07:50 -0300

    mejorando app

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 import {jwtVerify} from 'jose';
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const isToken = request.cookies.get('token')?.value;
   // if (!isToken) {
   //   return NextResponse.redirect(new URL('/signin', request.url));

c681bbe82e8d64ad17fbf0fd3cfbe7ac34183a03
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-04 11:11:19 -0300

    mejorando app

--- middleware.ts
+++ middleware.ts
@@ -3,24 +3,24 @@ import type {NextRequest} from 'next/server';
 import {jwtVerify} from 'jose';
 export async function middleware(request: NextRequest) {
   const isToken = request.cookies.get('token')?.value;
-  if (!isToken) {
-    return NextResponse.redirect(new URL('/signin', request.url));
-  }
-  const {payload} = await jwtVerify(
-    isToken,
-    new TextEncoder().encode(process.env.SECRECT)
-  );
+  // if (!isToken) {
+  //   return NextResponse.redirect(new URL('/signin', request.url));
+  // }
+  // const {payload} = await jwtVerify(
+  //   isToken,
+  //   new TextEncoder().encode(process.env.SECRECT)
+  // );
 
-  if (!payload) {
-    return NextResponse.redirect(new URL('/signin', request.url));
-  }
+  // if (!payload) {
+  //   return NextResponse.redirect(new URL('/signin', request.url));
+  // }
 
-  if (
-    request.nextUrl.pathname == '/signin' ||
-    request.nextUrl.pathname == '/signup'
-  ) {
-    return NextResponse.redirect(new URL('/home', request.url));
-  }
+  // if (
+  //   request.nextUrl.pathname == '/signin' ||
+  //   request.nextUrl.pathname == '/signup'
+  // ) {
+  //   return NextResponse.redirect(new URL('/home', request.url));
+  // }
 
   return NextResponse.next();
 }

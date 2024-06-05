dd9905c7f7169a7852cccac3ccb89ea0e4cc5444
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-05 09:48:03 -0300

    mejorando app

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,38 @@
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 import {jwtVerify} from 'jose';
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const isToken = request.cookies.get('token')?.value;
-  // if (!isToken) {
-  //   return NextResponse.redirect(new URL('/signin', request.url));
-  // }
-  // const {payload} = await jwtVerify(
-  //   isToken,
-  //   new TextEncoder().encode(process.env.SECRECT)
-  // );
+  if (!isToken) {
+    return NextResponse.redirect(new URL('/signin', request.url));
+  }
+  const {payload} = await jwtVerify(
+    isToken,
+    new TextEncoder().encode(process.env.SECRECT)
+  );
 
-  // if (!payload) {
-  //   return NextResponse.redirect(new URL('/signin', request.url));
-  // }
+  if (!payload) {
+    return NextResponse.redirect(new URL('/signin', request.url));
+  }
 
-  // if (
-  //   request.nextUrl.pathname == '/signin' ||
-  //   request.nextUrl.pathname == '/signup'
-  // ) {
-  //   return NextResponse.redirect(new URL('/home', request.url));
-  // }
+  if (
+    request.nextUrl.pathname == '/signin' ||
+    request.nextUrl.pathname == '/signup'
+  ) {
+    return NextResponse.redirect(new URL('/home', request.url));
+  }
 
   return NextResponse.next();
 }
 
-// export const config = {
-//   matcher: [
-//     '/home',
-//     '/amigos/:path*',
-//     '/configuracion',
-//     '/mensaje',
-//     '/notificaciones/:path*',
-//     '/perfil',
-//     '/search',
-//   ],
-// };
+export const config = {
+  matcher: [
+    '/home',
+    '/amigos/:path*',
+    '/configuracion',
+    '/mensaje',
+    '/notificaciones/:path*',
+    '/perfil',
+    '/search',
+  ],
+};

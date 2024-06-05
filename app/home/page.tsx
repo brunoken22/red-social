9db83d6b93b5c33d028031e1e1c7bd2e46fc469b
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-05 19:53:05 -0300

    probando

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,13 @@
-'use client';
 import {DivPublicaciones} from '@/ui/container';
 import {Publicar} from '@/components/publicar';
 import {Span} from '@/components/span';
-// import {Metadata} from 'next';
+import {Metadata} from 'next';
 import {PublicacionesAll} from '@/components/publicaciones/publicationsAll';
 
-// export const metadata: Metadata = {
-//   title: 'Home | UniRed',
-//   description: 'Inicio',
-// };
+export const metadata: Metadata = {
+  title: 'Home | UniRed',
+  description: 'Inicio',
+};
 export default function Home() {
   return (
     <main>

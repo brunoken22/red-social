0a1c9352d08c4f9d9ed2c146e19bf8890ad49ce6
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-05 12:32:59 -0300

    probando middleware

--- middleware.ts
+++ middleware.ts
@@ -16,25 +16,25 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/signin', request.url));
   }
   console.log(request.nextUrl.pathname);
-  // if (
-  //   request.nextUrl.pathname == '/signin' ||
-  //   request.nextUrl.pathname == '/signup'
-  // ) {
-  //   return NextResponse.redirect(new URL('/home', request.url));
-  // }
-  const response = NextResponse.next();
-  response.cookies.set('token', isToken);
-  return response;
+  if (
+    request.nextUrl.pathname == '/signin' ||
+    request.nextUrl.pathname == '/signup'
+  ) {
+    return NextResponse.redirect(new URL('/home', request.url));
+  }
+  return NextResponse.next();
+  // response.cookies.set('token', isToken);
+  //  response;
 }
 
-export const config = {
-  matcher: [
-    '/home',
-    '/amigos/:path*',
-    '/configuracion',
-    '/mensaje',
-    '/notificaciones/:path*',
-    '/perfil',
-    '/search',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/home',
+//     '/amigos/:path*',
+//     '/configuracion',
+//     '/mensaje',
+//     '/notificaciones/:path*',
+//     '/perfil',
+//     '/search',
+//   ],
+// };

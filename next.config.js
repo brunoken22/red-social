eb5f62c3e47b00c7cb8bc4a5584ca04217d1e685
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-05 08:31:42 -0300

    mejorando app

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const withSvgr = require("next-svgr");
  
 const nextConfig = withSvgr({
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['my-proxy.com', '*.my-proxy.com'],
-    },
-  },
   images: {
     domains: ['res.cloudinary.com'], // Agrega los dominios permitidos aquí
   },

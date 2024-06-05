9e40b30ec2f719e9edef601aefd27cc0def5d6e5
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-05 16:12:18 -0300

    mejorando app

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,14 @@ import {NextRequest} from 'next/server';
 import {jwtVerify} from 'jose';
 export async function middleware(request: NextRequest) {
   const isToken = request.cookies.get('token')?.value;
+  console.log(isToken, request.nextUrl.pathname);
   if (!isToken) {
-    // if (
-    //   request.nextUrl.pathname !== '/signin' &&
-    //   request.nextUrl.pathname !== '/signup'
-    // ) {
-    //   return NextResponse.redirect(new URL('/signin', request.url));
-    // }
+    if (
+      request.nextUrl.pathname !== '/signin' &&
+      request.nextUrl.pathname !== '/signup'
+    ) {
+      return NextResponse.redirect(new URL('/signin', request.url));
+    }
     return NextResponse.next();
   } else {
     const {payload} = await jwtVerify(
@@ -30,16 +31,16 @@ export async function middleware(request: NextRequest) {
   }
 }
 
-// export const config = {
-//   matcher: [
-//     '/home',
-//     '/amigos/:path*',
-//     '/configuracion',
-//     '/mensaje',
-//     '/notificaciones/:path*',
-//     '/perfil',
-//     '/search',
-//     '/signin',
-//     '/signup',
-//   ],
-// };
+export const config = {
+  matcher: [
+    '/home',
+    '/amigos/:path*',
+    '/configuracion',
+    '/mensaje',
+    '/notificaciones/:path*',
+    '/perfil',
+    '/search',
+    '/signin',
+    '/signup',
+  ],
+};

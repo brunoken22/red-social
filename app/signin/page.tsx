59b52a1775ebbbea1e4586b3125314c1c2e78c86
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-06 17:09:41 -0300

    probando

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -23,6 +23,7 @@ export default function Page() {
               Registrate
             </Link>
           </p>
+          <Link href={'/home'}>Home</Link>
         </div>
       </div>
     </div>

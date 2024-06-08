44bcdf973e3c307724d4fc8c2ed4b3f96e05477c
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-08 17:25:36 -0300

    probando

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@ const withSvgr = require("next-svgr");
  
 const nextConfig = withSvgr({
   reactStrictMode: true,
+  // crossOrigin: 'anonymous',
   async rewrites() {
     return [ 
       {
         source: '/:path*',
         destination: 'https://red-social-node.onrender.com/:path*', 
+      },
+      {
+        source: '/:path*',
+        destination: 'http://localhost:3000/:path*', 
       },
     ];
   },

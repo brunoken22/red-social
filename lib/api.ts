c635abcb672ba167fbf82bd9b75bdace22ccad5b
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-14 19:19:13 -0300

    delete console api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,3 @@
-import {getTokenCookie} from './cookie';
-
 const Api_url = process.env.NEXT_PUBLIC_PORT;
 export async function fetchApiSwr(api: string, option: any) {
   const response = await fetch(`${Api_url}/api${api}`, option);

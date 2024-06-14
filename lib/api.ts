520fe4c4d18121c9c090dd57c9751dc7ad0e0736
Bruno Ken <bruno_am_22@hotmail.com>
2024-06-14 19:19:34 -0300

    delete api.ts console

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,5 @@ const Api_url = process.env.NEXT_PUBLIC_PORT;
 export async function fetchApiSwr(api: string, option: any) {
   const response = await fetch(`${Api_url}/api${api}`, option);
   const data = await response.json();
-  console.log(data);
   return data;
 }

14299daae7d1f56a22888a5ac2dc5e7220a9324e
Bruno Ken <bruno_am_22@hotmail.com>
2024-09-03 14:29:56 -0300

    cambiando etiqueta next Link

--- utils/formtText.tsx
+++ utils/formtText.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Linkify({text}: {text: string}) {
   const urlPattern =
     /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
@@ -7,18 +9,17 @@ export default function Linkify({text}: {text: string}) {
   return (
     <>
       {parts.map((part, index) => {
-
         if (urlPattern.test(part)) {
           const domainName = part.replace(/(^\w+:|^)\/\//, '').split('/')[0];
           return (
-            <a
+            <Link
               key={index}
               href={part}
               target='_blank'
               rel='noopener noreferrer'
               className='text-[rgb(59_184_246/87%)] hover:opacity-60'>
               {domainName}
-            </a>
+            </Link>
           );
         } else {
           return <span key={index}>{part == 'https' ? '' : part}</span>;

f8da0ac9950c013c4517cd6cdddd03c8e9777b17
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 19:56:27 -0300

    intento

--- app/amigos/[id]/page.tsx
+++ app/amigos/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { PerfilAmigo } from '@/components/perfilAmigo';
 import { Main } from '@/ui/container';
+import Head from 'next/head';
 // import { Metadata } from 'next';
 
 // export const metadata: Metadata = {
@@ -10,6 +11,26 @@ import { Main } from '@/ui/container';
 export default function () {
   return (
     <>
+      <Head>
+        <head>
+          <meta name='google-site-verification' content='CnmK8AWJQTO2MYQ5J7dOu9_dhCFy-ttErrYHDEWbOyw' />
+          <link rel='preconnect' href='https://red-social-node.onrender.com' />
+          <link rel='preconnect' href='https://res.cloudinary.com' />
+          <meta name='color-scheme' content='only light' />
+
+          <meta property='og:url' content='https://unired.vercel.app/amigos/60' />
+          <meta property='og:type' content='website' />
+          <meta property='og:title' content='Fache' />
+          <meta property='og:description' content='Usuario Fache de unired' />
+          <meta property='og:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta property='twitter:domain' content='unired.vercel.app' />
+          <meta property='twitter:url' content='https://unired.vercel.app/amigos/60' />
+          <meta name='twitter:title' content='Fache' />
+          <meta name='twitter:description' content='Usuario Fache de unired' />
+          <meta name='twitter:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
+        </head>
+      </Head>
       <Main>
         <PerfilAmigo />
       </Main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,18 +22,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <link rel='preconnect' href='https://red-social-node.onrender.com' />
         <link rel='preconnect' href='https://res.cloudinary.com' />
         <meta name='color-scheme' content='only light' />
-
-        <meta property='og:url' content='https://unired.vercel.app/amigos/60' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Fache' />
-        <meta property='og:description' content='Usuario Fache de unired' />
-        <meta property='og:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta property='twitter:domain' content='unired.vercel.app' />
-        <meta property='twitter:url' content='https://unired.vercel.app/amigos/60' />
-        <meta name='twitter:title' content='Fache' />
-        <meta name='twitter:description' content='Usuario Fache de unired' />
-        <meta name='twitter:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
       </head>
       <body className={`${poppins.className} dark:bg-dark dark:text-white dark:transition-dark`}>
         <Layout themeDate={theme}>{children}</Layout>

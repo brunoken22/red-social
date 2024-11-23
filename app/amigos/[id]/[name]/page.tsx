dcf3bcbe68c4e8efcaff412135b5cb11fc55b399
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 15:51:44 -0300

    intento

--- app/amigos/[id]/[name]/page.tsx
+++ app/amigos/[id]/[name]/page.tsx
@@ -3,9 +3,9 @@ import { Main } from '@/ui/container';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'UniRed',
-  description: 'Red social',
-  keywords: 'La red social líder para conectar con amigos, compartir fotos. Únete hoy mismo y forma parte de nuestra comunidad.',
+  // title: 'UniRed',
+  // description: 'Red social',
+  // keywords: 'La red social líder para conectar con amigos, compartir fotos. Únete hoy mismo y forma parte de nuestra comunidad.',
   openGraph: {
     title: 'Mi Página Especial',
     description: 'Descripción única de mi página especial.',

01779937a9c853734a3d6c3ee9286a22b1ce625d
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 20:10:48 -0300

    intento

--- app/amigos/[id]/page.tsx
+++ app/amigos/[id]/page.tsx
@@ -1,16 +1,39 @@
 import { PerfilAmigo } from '@/components/perfilAmigo';
 import { Main } from '@/ui/container';
-// import { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
-// export const metadata: Metadata = {
-//   title: 'Amigos | UniRed',
-//   description: 'Amigos',
-// };
+export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  // read route params
+  const id = (await params).id;
+
+  // fetch data
+  const response = await fetch(`${process.env.NEXT_PUBLIC_PORT}/api/user/amigos/` + id, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+  const data = await response.json();
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: data.user.fullName + ' | UniRed' || 'Unired',
+    openGraph: {
+      images: ['/user.webp', ...previousImages],
+    },
+  };
+}
 
 export default function () {
   return (
     <>
-      <head>
+      {/* <head>
         <title>data.user.fullName</title>
         <meta property='og:url' content='https://unired.vercel.app/amigos/60' />
         <meta property='og:type' content='website' />
@@ -23,7 +46,7 @@ export default function () {
         <meta name='twitter:title' content='Fache' />
         <meta name='twitter:description' content='Usuario Fache de unired' />
         <meta name='twitter:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
-      </head>
+      </head> */}
       <Main>
         <PerfilAmigo />
       </Main>

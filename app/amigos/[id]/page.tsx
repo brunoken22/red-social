79b41b493209f22d18470831dc745c2705b72b9f
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 16:17:56 -0300

    intento

--- app/amigos/[id]/page.tsx
+++ app/amigos/[id]/page.tsx
@@ -6,15 +6,15 @@ export const metadata: Metadata = {
   // title: 'UniRed',
   description: 'Red social',
   keywords: 'La red social líder para conectar con amigos, compartir fotos. Únete hoy mismo y forma parte de nuestra comunidad.',
-  openGraph: {
-    title: 'Mi Página Especial',
-    description: 'Descripción única de mi página especial.',
-    type: 'website',
-    images: '/user.webp',
-  },
+  // openGraph: {
+  //   title: 'Mi Página Especial',
+  //   description: 'Descripción única de mi página especial.',
+  //   type: 'website',
+  //   images: '/user.webp',
+  // },
 };
 
-export default function UsuarioId({ params }: { params: { name: string } }) {
+export default function () {
   return (
     <>
       <Main>

3a2ffbe33c85bd17e18507fa9ae7c950988a6069
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 15:39:42 -0300

    intento

--- app/amigos/[id]/[name]/page.tsx
+++ app/amigos/[id]/[name]/page.tsx
@@ -5,6 +5,11 @@ export default function UsuarioId({ params }: { params: { name: string } }) {
   return (
     <>
       <title>{decodeURIComponent(params.name) + ' | UniRed '}</title>
+      <meta property='og:title' content='Mi Página Especial' />
+      <meta property='og:description' content='Descripción única de mi página especial.' />
+      <meta property='og:image' content='https://example.com/imagen-unica.jpg' />
+      <meta property='og:url' content='https://example.com/mipagina' />
+      <meta property='og:type' content='website' />
       <Main>
         <PerfilAmigo />
       </Main>

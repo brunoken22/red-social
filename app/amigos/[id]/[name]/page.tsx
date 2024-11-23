07d26051d8ad636ea83911a05f1647720bfca431
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 15:45:01 -0300

    intento

--- app/amigos/[id]/[name]/page.tsx
+++ app/amigos/[id]/[name]/page.tsx
@@ -1,15 +1,23 @@
 import { PerfilAmigo } from '@/components/perfilAmigo';
 import { Main } from '@/ui/container';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'UniRed',
+  description: 'Red social',
+  keywords: 'La red social líder para conectar con amigos, compartir fotos. Únete hoy mismo y forma parte de nuestra comunidad.',
+  openGraph: {
+    title: 'Mi Página Especial',
+    description: 'Descripción única de mi página especial.',
+    type: 'website',
+    images: '/user.webp',
+  },
+};
 
 export default function UsuarioId({ params }: { params: { name: string } }) {
   return (
     <>
       <title>{decodeURIComponent(params.name) + ' | UniRed '}</title>
-      <meta property='og:title' content='Mi Página Especial' />
-      <meta property='og:description' content='Descripción única de mi página especial.' />
-      <meta property='og:image' content='https://example.com/imagen-unica.jpg' />
-      <meta property='og:url' content='https://example.com/mipagina' />
-      <meta property='og:type' content='website' />
       <Main>
         <PerfilAmigo />
       </Main>

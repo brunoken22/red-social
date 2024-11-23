e2875d7ba67215fe2a1b9d80b831240d24a48575
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 16:42:19 -0300

    intento

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,15 @@
-import {NextResponse} from 'next/server';
-import {NextRequest} from 'next/server';
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 export async function middleware(request: NextRequest) {
   try {
     const isToken = request.cookies.get('login')?.value;
     if (isToken !== 'true') {
-      if (
-        request.nextUrl.pathname !== '/iniciarSesion' &&
-        request.nextUrl.pathname !== '/' &&
-        request.nextUrl.pathname !== '/crearCuenta'
-      ) {
+      if (request.nextUrl.pathname !== '/iniciarSesion' && request.nextUrl.pathname !== '/' && request.nextUrl.pathname !== '/crearCuenta') {
         return NextResponse.redirect(new URL('/', request.url));
       }
       return NextResponse.next();
     } else {
-      if (
-        request.nextUrl.pathname === '/iniciarSesion' ||
-        request.nextUrl.pathname === '/crearCuenta' ||
-        request.nextUrl.pathname === '/'
-      ) {
+      if (request.nextUrl.pathname === '/iniciarSesion' || request.nextUrl.pathname === '/crearCuenta' || request.nextUrl.pathname === '/') {
         return NextResponse.redirect(new URL('/inicio', request.url));
       }
       return NextResponse.next();
@@ -29,7 +21,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/inicio',
-    '/amigos/:path*',
+    // '/amigos/:path*',
     '/configuracion',
     '/chat',
     '/notificaciones/:path*',

a452b48054bf207d234849be8713022a4d1474a5
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-23 20:00:10 -0300

    intento

--- app/amigos/[id]/page.tsx
+++ app/amigos/[id]/page.tsx
@@ -12,24 +12,18 @@ export default function () {
   return (
     <>
       <Head>
-        <head>
-          <meta name='google-site-verification' content='CnmK8AWJQTO2MYQ5J7dOu9_dhCFy-ttErrYHDEWbOyw' />
-          <link rel='preconnect' href='https://red-social-node.onrender.com' />
-          <link rel='preconnect' href='https://res.cloudinary.com' />
-          <meta name='color-scheme' content='only light' />
-
-          <meta property='og:url' content='https://unired.vercel.app/amigos/60' />
-          <meta property='og:type' content='website' />
-          <meta property='og:title' content='Fache' />
-          <meta property='og:description' content='Usuario Fache de unired' />
-          <meta property='og:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta property='twitter:domain' content='unired.vercel.app' />
-          <meta property='twitter:url' content='https://unired.vercel.app/amigos/60' />
-          <meta name='twitter:title' content='Fache' />
-          <meta name='twitter:description' content='Usuario Fache de unired' />
-          <meta name='twitter:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
-        </head>
+        <title>{'data.user.fullName'} </title>
+        <meta property='og:url' content='https://unired.vercel.app/amigos/60' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Fache' />
+        <meta property='og:description' content='Usuario Fache de unired' />
+        <meta property='og:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='unired.vercel.app' />
+        <meta property='twitter:url' content='https://unired.vercel.app/amigos/60' />
+        <meta name='twitter:title' content='Fache' />
+        <meta name='twitter:description' content='Usuario Fache de unired' />
+        <meta name='twitter:image' content='https://res.cloudinary.com/dy26iktoi/image/upload/v1722007305/xcxvdpo8k2vma5okxx8a.webp' />
       </Head>
       <Main>
         <PerfilAmigo />

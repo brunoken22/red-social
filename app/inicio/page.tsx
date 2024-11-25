a977b4c846ac1679aaa77d988dccb7362aaadc08
Bruno Ken <bruno_am_22@hotmail.com>
2024-11-25 10:58:03 -0300

    mejorando diseño

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -1,15 +1,13 @@
 import dynamic from 'next/dynamic';
-import {DivPublicaciones} from '@/ui/container';
-import {Span} from '@/components/span';
-import {Metadata} from 'next';
+import { DivPublicaciones } from '@/ui/container';
+import { Span } from '@/components/span';
+import { Metadata } from 'next';
 const Publicar = dynamic(() => import('@/components/publicar'));
-const PublicacionesAll = dynamic(
-  () => import('@/components/publicaciones/publicationsAll')
-);
+const PublicacionesAll = dynamic(() => import('@/components/publicaciones/publicationsAll'));
 
 export const metadata: Metadata = {
   title: 'Inicio | UniRed',
-  description: 'Inicio',
+  description: 'Inicio de unired',
 };
 export default function Home() {
   return (

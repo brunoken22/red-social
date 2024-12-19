3439b8153d8934bfe3fb6b72d17173b0b97f6856
Bruno Ken <bruno_am_22@hotmail.com>
2024-12-19 15:45:59 -0300

    mejorando

--- components/configuracionPerfil/styled.tsx
+++ components/configuracionPerfil/styled.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export const DivConfiguracionPerfil = ({ children }: { children: React.ReactNode }) => (
-  <div className='w-3/5 flex justify-evenly gap-4 max-md:flex-col max-md:items-center'>{children}</div>
+  <div className='w-3/5 max-md:w-full flex justify-evenly gap-4 max-md:flex-col max-md:items-center'>
+    {children}
+  </div>
 );
 
 export const DIvCongifurar = ({ children }: { children: React.ReactNode }) => (
-  <div className='max-w-[60%] w-full h-full m-auto max-md:text-center max-md:mt-6 max-md:max-w-full'>{children}</div>
+  <div className='max-w-[60%] w-full h-full m-auto max-md:text-center max-md:mt-6 max-md:max-w-full'>
+    {children}
+  </div>
 );

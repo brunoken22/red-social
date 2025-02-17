8b33df99b2a37d5246c97c7579c63988b46115c9
Bruno Ken <bruno_am_22@hotmail.com>
2025-02-17 11:18:27 -0300

    cambiando color de connect

--- ui/FotoPerfil/index.tsx
+++ ui/FotoPerfil/index.tsx
@@ -1,8 +1,30 @@
-export default function FotoPerfil({ className, img, connect, isBorder = false }: { className: string; img: string | undefined; connect?: boolean | undefined; isBorder?: boolean }) {
+export default function FotoPerfil({
+  className,
+  img,
+  connect,
+  isBorder = false,
+}: {
+  className: string;
+  img: string | undefined;
+  connect?: boolean | undefined;
+  isBorder?: boolean;
+}) {
   return (
     <div className={` relative shrink-0`}>
-      <img src={(img == 'false' && '/user.webp') || img || '/user.webp'} height={40} width={40} className={`${className ? className : 'w-[40px] h-[40px]'} rounded-full object-cover ${isBorder ? 'border-4' : ''} dark:border-darkComponet border-inherit`} alt='perfil' loading='lazy' />
-      {connect ? <div className={` bg-green-800 w-[11px] h-[11px] rounded-full absolute bottom-[10%] right-0`}></div> : null}
+      <img
+        src={(img == 'false' && '/user.webp') || img || '/user.webp'}
+        height={40}
+        width={40}
+        className={`${className ? className : 'w-[40px] h-[40px]'} rounded-full object-cover ${
+          isBorder ? 'border-4' : ''
+        } dark:border-darkComponet border-inherit`}
+        alt='perfil'
+        loading='lazy'
+      />
+      {connect ? (
+        <div
+          className={` bg-green-500 w-[11px] h-[11px] rounded-full absolute bottom-[10%] right-0`}></div>
+      ) : null}
     </div>
   );
 }

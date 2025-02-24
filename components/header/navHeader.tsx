8c27fa7ea049c25584fad7b8895c088ec42567fe
Bruno Ken <bruno_am_22@hotmail.com>
2025-02-24 10:44:26 -0300

    mejorando diseño

--- components/header/navHeader.tsx
+++ components/header/navHeader.tsx
@@ -1,11 +1,11 @@
-import {usePathname} from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Home from '@/ui/icons/home.svg';
 import Amigos from '@/ui/icons/amigos.svg';
 import Chat from '@/ui/icons/chat.svg';
 import Notificaciones from '@/ui/icons/notificaciones.svg';
 import Search from '@/ui/icons/search.svg';
-import {DivNotificacionActi} from './styled';
+import { DivNotificacionActi } from './styled';
 export default function NavegationUrl({
   amigos,
   message,
@@ -17,49 +17,59 @@ export default function NavegationUrl({
 }) {
   const pathname = usePathname();
   const className = 'relative';
+  const isOpen = 'p-2 bg-light rounded-full';
   return (
     <div className='flex justify-evenly items-center gap-8 max-md:gap-4'>
       <Link href={'/search'} aria-label='search'>
-        <span className='hidden  max-md:block'>
+        <span className={`hidden  max-md:block ${pathname == '/search' ? isOpen : ''} `}>
           <Search
             className={`${
               pathname == '/search'
-                ? 'fill-unired '
+                ? 'fill-white w-[20px] h-[20px]'
                 : 'hover:opacity-70 dark:fill-gray-200 fill-gray-600 '
             }`}
           />
         </span>
       </Link>
-      <Link href={'/inicio'} aria-label='home' className={className}>
-        <span className={``}>
+      <Link
+        href={'/inicio'}
+        aria-label='home'
+        className={`${pathname == '/inicio' ? isOpen : ''} ${className}`}>
+        <span>
           <Home
             className={`${
               pathname == '/inicio'
-                ? 'fill-unired '
+                ? 'fill-white w-[20px] h-[20px]'
                 : 'hover:opacity-70 dark:fill-gray-200 fill-gray-600 '
             }`}
           />
         </span>
       </Link>
-      <Link href={'/amigos'} aria-label='amigos' className={className}>
+      <Link
+        href={'/amigos'}
+        aria-label='amigos'
+        className={`${pathname == '/amigos' ? isOpen : ''} ${className}`}>
         {amigos ? <DivNotificacionActi>{amigos}</DivNotificacionActi> : null}
-        <span className={``}>
+        <span>
           <Amigos
             className={`${
               pathname == '/amigos'
-                ? 'fill-unired '
-                : 'hover:opacity-70 dark:fill-gray-200 fill-gray-600 '
+                ? ' fill-white  w-[20px] h-[20px]'
+                : 'hover:opacity-70 dark:fill-gray-200 fill-gray-600'
             }`}
           />
         </span>
       </Link>
-      <Link href={'/chat'} aria-label='mensaje' className={className}>
+      <Link
+        href={'/chat'}
+        aria-label='mensaje'
+        className={`${pathname == '/chat' ? isOpen : ''} ${className}`}>
         {message > 0 && <DivNotificacionActi>{message}</DivNotificacionActi>}
         <span>
           <Chat
             className={`${
               pathname == '/chat'
-                ? 'fill-unired '
+                ? 'fill-white w-[20px] h-[20px]'
                 : 'hover:opacity-70 dark:fill-gray-200 fill-gray-600 '
             }`}
           />{' '}
@@ -68,15 +78,13 @@ export default function NavegationUrl({
       <Link
         href={'/notificaciones'}
         aria-label='notificaciones'
-        className={className}>
-        {notification ? (
-          <DivNotificacionActi>{notification}</DivNotificacionActi>
-        ) : null}
+        className={`${pathname == '/notificaciones' ? isOpen : ''} ${className}`}>
+        {notification ? <DivNotificacionActi>{notification}</DivNotificacionActi> : null}
         <span>
           <Notificaciones
             className={`${
               pathname == '/notificaciones'
-                ? 'fill-unired '
+                ? 'fill-white  w-[20px] h-[20px]'
                 : 'hover:opacity-70 dark:fill-gray-200 fill-gray-600 '
             }`}
           />{' '}

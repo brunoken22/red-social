736fbb6be9c78bb3a71e1752bad6180c8ca3c504
Bruno Ken <bruno_am_22@hotmail.com>
2025-02-24 21:00:31 -0300

    mejroando notificaciones

--- components/templateNotificacion/notificationId.tsx
+++ components/templateNotificacion/notificationId.tsx
@@ -7,6 +7,7 @@ import { useRecoilValue } from 'recoil';
 import { useParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { LoaderRequest } from '../loader';
+import Link from 'next/link';
 
 const ThemplatePubli = dynamic(
   () => import('../templatePublicate').then((mod) => mod.ThemplatePubli),
@@ -28,8 +29,8 @@ export default function NotificationId() {
 
   return (
     <div className='max-w-[600px] w-full'>
-      <DivAllPublicaciones>
-        {dataPubliId && (
+      {dataPubliId ? (
+        <DivAllPublicaciones>
           <ThemplatePubli
             mutate={mutatePublicacionesUser}
             vereficationUser={dataUser.user?.verification}
@@ -43,8 +44,22 @@ export default function NotificationId() {
             idPublicacion={dataPubliId.id}
             userId={dataUser.user.id}
             user={dataPubliId.user}></ThemplatePubli>
-        )}
-      </DivAllPublicaciones>
+        </DivAllPublicaciones>
+      ) : (
+        <div className='dark:bg-darkComponet  shadow-lg rounded-2xl p-8 max-w-md'>
+          <h1 className='text-2xl font-bold dark:text-primary text-secundary  mb-4'>
+            ⚠️ Publicación no encontrada
+          </h1>
+          <p className='dark:text-primary text-secundary mb-6'>
+            Esta publicación no se puede visualizar. Es posible que haya sido eliminada o no exista.
+          </p>
+          <Link
+            href='/notificaciones'
+            className='bg-light  text-primary px-4 py-2 rounded-lg hover:bg-blue-600 transition'>
+            Volver a tus notificaciones
+          </Link>
+        </div>
+      )}
     </div>
   );
 }

8334e7336d997077db42d371c66cf2f4f00acd4b
Bruno Ken <bruno_am_22@hotmail.com>
2025-05-11 19:51:08 -0300

    agregandi mejor description alt

--- ui/logo/index.tsx
+++ ui/logo/index.tsx
@@ -1,11 +1,7 @@
 export default function LogoPage() {
   return (
     <div className={`flex items-center justify-center font-bold `}>
-      <img
-        src='/logo.webp'
-        alt='uniRed'
-        className='w-[40px] rounded-[50%] mr-2'
-      />
+      <img src='/logo.webp' alt='logom de uniRed' className='w-[40px] rounded-[50%] mr-2' />
       <p>UniRed</p>
     </div>
   );

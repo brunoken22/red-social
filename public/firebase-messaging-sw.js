5a2d4e1beeb9bfffa2c8120fc9e77e7d0e4fda6f
Bruno Ken <bruno_am_22@hotmail.com>
2025-05-11 17:18:39 -0300

    itnetnado cambiar el firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,14 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-comp
 
 // Tu configuración de Firebase
 firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDY0au5x6QgwjHw8UhpBEJDVC6-ASKAOFI',
+  authDomain: 'unired-55098.firebaseapp.com',
+  projectId: 'unired-55098',
+  storageBucket: 'unired-55098.firebasestorage.app',
+  messagingSenderId: '1062171130331',
+  appId: '1:1062171130331:web:f54f6222788d1627124ffe',
+  measurementId: 'G-0CW2WETLQ2',
 });
 
 // Inicializa el servicio de mensajería

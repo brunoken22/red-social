2ec85228be141e3409f995c1cf6a1fa418c3ee56
Bruno Ken <bruno_am_22@hotmail.com>
2025-05-14 12:06:45 -0300

    probando

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://unired.vercel.app/sitemap.xml',
+    sitemap: 'https://unired.vercel.app/sitemap-unired.xml',
   };
 }
